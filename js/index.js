ea781d502064ba60ba3f65a9b6e969ca943c30f0
ZhangYu <1757007328@qq.com>
2017-08-24 22:52:03 +0800

    day2-2

--- js/index.js
+++ js/index.js
@@ -1,31 +1,31 @@
 //轮播js
-$(function(){
-	var i=0;
-	var iTem = setInterval(function(){
-			move();
-		},3000);
-	function move(){
+$(function() {
+	var i = 0;
+	var iTem = setInterval(function() {
+		move();
+	}, 3000);
+
+	function move() {
 		i++;
-		if(i == 5){
+		if(i == 5) {
 			i = 0;
 		}
 		$('.lunbo-pic li').eq(i).fadeIn(500).siblings().fadeOut(500);
 		$('.lunbo-nav li').eq(i).addClass('select').siblings().removeClass('select');
 	}
-	
-	
-	$('.lunbo-nav li').hover(function(){
+
+	$('.lunbo-nav li').hover(function() {
 		var j = $(this).index();
 		clearInterval(iTem);
-			$('.lunbo-pic li').eq(j).fadeIn(500).siblings().fadeOut(500);
-			$('.lunbo-nav li').eq(j).addClass('select').siblings().removeClass('select');
-		})
-	
-	$('.lunbo-pic li').hover(function(){
+		$('.lunbo-pic li').eq(j).fadeIn(500).siblings().fadeOut(500);
+		$('.lunbo-nav li').eq(j).addClass('select').siblings().removeClass('select');
+	})
+
+	$('.lunbo-pic li').hover(function() {
 		clearInterval(iTem);
-	},function(){
-		iTem = setInterval(function(){
+	}, function() {
+		iTem = setInterval(function() {
 			move();
-		},3000);
+		}, 3000);
 	})
 })
