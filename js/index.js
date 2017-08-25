8351e2f5ee972732155f3991658017d1929690e1
ZhangYu <1757007328@qq.com>
2017-08-25 21:46:11 +0800

    day3-2

--- js/index.js
+++ js/index.js
@@ -27,5 +27,34 @@ $(function() {
 		iTem = setInterval(function() {
 			move();
 		}, 3000);
+	});
+	
+
+	
+	
+	$("#index-nav-list ul").on("mouseenter","li",function(){
+		$(".navCon").show();
+		var index=$(this).index();
+		$.get("json/nav_data.json",function(data){
+			var html = template("navCon",data[index]);
+			$(".navCon").html(html);
+			
+		})
+		
+	});
+	
+	
+	
+	
+	
+	$("#index-nav-list").on("mouseleave",function(){
+		$(".navCon").hide();
 	})
+	
+	$.get("json/nav.json",function(data){
+		var html = template("nav",data);
+		$("#index-nav-list ul").html(html);
+	})
+	
+	
 })
